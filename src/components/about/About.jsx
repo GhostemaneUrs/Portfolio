f0c5da0e0200705d2cd14e7d676c0b2b772570e3
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-03-12 10:58:42 -0500

    feat: floating nav and about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,48 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import aboutImg from "../../assets/img/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
     <section id="about">
-      About
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className={styles.container_about}>
+        <div className={styles.about_me}>
+          <div className={styles.about_me_image}>
+            <img src={aboutImg} alt="about image" />
+          </div>
+        </div>
+        <div className={styles.about_content}>
+          <div className={styles.about_cards}>
+            <div className={styles.card}>
+              <FaAward className={styles.about_icon} />
+              <h5>Experience</h5>
+              <span>8 Months Working</span>
+            </div>
+            <div className={styles.card}>
+              <VscFolderLibrary className={styles.about_icon} />
+              <h5>Projects</h5>
+              <span>13 Completed</span>
+            </div>
+          </div>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+            recusandae natus fugit sit voluptatem voluptatum et fuga, quos
+            incidunt quisquam sequi laudantium ipsa, error earum aut accusamus
+            pariatur dignissimos odit!
+          </p>
+          <a
+            href="#contact"
+            className={`${styles.button} ${styles.button_primary}`}
+          >
+            Let's Talk
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,25 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.scss";
-import { RiServiceLine } from "react-icons/ri";
 import { BiMessageSquareDetail } from "react-icons/bi";
 import { AiOutlineHome, AiOutlineUser, AiOutlineBook } from "react-icons/ai";
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
+
   return (
-    <div>
-      <a href="#" className={styles.icon_socials}>
+    <div className={styles.container_nav}>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={
+          activeNav === "#"
+            ? `${styles.active} ${styles.icon_socials}`
+            : `${styles.icon_socials}`
+        }
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about" className={styles.icon_socials}>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={
+          activeNav === "#about"
+            ? `${styles.active} ${styles.icon_socials}`
+            : `${styles.icon_socials}`
+        }
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience" className={styles.icon_socials}>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={
+          activeNav === "#experience"
+            ? `${styles.active} ${styles.icon_socials}`
+            : `${styles.icon_socials}`
+        }
+      >
         <AiOutlineBook />
       </a>
-      <a href="#services" className={styles.icon_socials}>
-        <RiServiceLine />
-      </a>
-      <a href="#contact" className={styles.icon_socials}>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={
+          activeNav === "#contact"
+            ? `${styles.active} ${styles.icon_socials}`
+            : `${styles.icon_socials}`
+        }
+      >
         <BiMessageSquareDetail />
       </a>
     </div>
