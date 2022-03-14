8c98ac3dff72391db50e9d8e92ad40c6a53167fb
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-03-14 07:50:26 -0500

    feat: update structure header and styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,38 +6,36 @@ import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <div className={styles.header_container}>
-        <h5>Hello I'm</h5>
-        <h1>Andres Jaramillo</h1>
-        <h5 className={styles.text_light}>FrontEnd Developer.</h5>
-        <div className={styles.container_button}>
-          <a href={cv} download className={styles.button}>
-            Download CV
-          </a>
-          <a href="" className={`${styles.button} ${styles.button_primary}`}>
-            Let's Talk
-          </a>
-        </div>
-        <div className={styles.socials}>
-          <a
-            href="https://www.linkedin.com/in/andres-jaramillo0406/"
-            className={styles.icon_socials}
-            target="_blank"
-          >
-            <BsLinkedin />
-          </a>
-          <a
-            href="https://github.com/GhostemaneUrs"
-            className={styles.icon_socials}
-            target="_blank"
-          >
-            <BsGithub />
-          </a>
-        </div>
-        <div className={styles.avatar}>
-          <img src={avatar} alt="img_avatar" />
-        </div>
+    <div className={styles.header_container}>
+      <h5>Hello I'm</h5>
+      <h1>Andres Jaramillo</h1>
+      <h5 className={styles.text_light}>FrontEnd Developer.</h5>
+      <div className={styles.container_button}>
+        <a href={cv} download className={styles.button}>
+          Download CV
+        </a>
+        <a href="" className={`${styles.button} ${styles.button_primary}`}>
+          Let's Talk
+        </a>
+      </div>
+      <div className={styles.socials}>
+        <a
+          href="https://www.linkedin.com/in/andres-jaramillo0406/"
+          className={styles.icon_socials}
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/GhostemaneUrs"
+          className={styles.icon_socials}
+          target="_blank"
+        >
+          <BsGithub />
+        </a>
+      </div>
+      <div className={styles.avatar}>
+        <img src={avatar} alt="img_avatar" />
       </div>
     </div>
   );
