1b372d468a88db502bb8d21a1b8a7a093ff53b12
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-03-14 07:50:11 -0500

    feat: experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,76 @@
-import React from 'react'
+import React from "react";
 import styles from "./styles.module.scss";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
-  )
-}
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
 
-export default Experience
+      <div className={styles.container_experience}>
+        <div className={styles.experience}>
+          <h3>FrontEnd Development </h3>
+          <div className={styles.experience_content}>
+            <div className={styles.details}>
+              <BsPatchCheckFill className={styles.experience_icon} />
+              <h4>HTML</h4>
+            </div>
+
+            <div className={styles.details}>
+              <BsPatchCheckFill className={styles.experience_icon} />
+              <h4>CSS</h4>
+            </div>
+
+            <div className={styles.details}>
+              <BsPatchCheckFill className={styles.experience_icon} />
+              <h4>React</h4>
+            </div>
+
+            <div className={styles.details}>
+              <BsPatchCheckFill className={styles.experience_icon} />
+              <h4>Bootstrap</h4>
+            </div>
+
+            <div className={styles.details}>
+              <BsPatchCheckFill className={styles.experience_icon} />
+              <h4>Tailwind</h4>
+            </div>
+
+            <div className={styles.details}>
+              <BsPatchCheckFill className={styles.experience_icon} />
+              <h4>JavaScript</h4>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.experience}>
+          <h3>BackEnd Development </h3>
+          <div className={styles.experience_content}>
+            <div className={styles.details}>
+              <BsPatchCheckFill className={styles.experience_icon} />
+              <h4>Docker</h4>
+            </div>
+
+            <div className={styles.details}>
+              <BsPatchCheckFill className={styles.experience_icon} />
+              <h4>Mysql</h4>
+            </div>
+
+            <div className={styles.details}>
+              <BsPatchCheckFill className={styles.experience_icon} />
+              <h4>Node Js</h4>
+            </div>
+
+            <div className={styles.details}>
+              <BsPatchCheckFill className={styles.experience_icon} />
+              <h4>TypeScript</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
